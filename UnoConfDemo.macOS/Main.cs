a50e13280284f6d661ae69af79c5e9b422bc74cd
Daniel Causer <daniel.causer@nventive.com>
2021-10-30 14:36:39 -0400

    Add project files.

---
+++ UnoConfDemo.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace UnoConfDemo.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
