0d38eea295b2ad31868fb64c69a1bbd0dca018d0
Daniel Causer <daniel.causer@nventive.com>
2021-10-30 17:09:11 -0400

    forgot to add the code for the command on the view model

--- UnoConfDemo.Shared/MainPageViewModel.cs
+++ UnoConfDemo.Shared/MainPageViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace UnoConfDemo
 {
@@ -8,9 +12,18 @@ namespace UnoConfDemo
     {
         private readonly IBarCodeReaderService _barCodeService;
 
+        public IAsyncRelayCommand ScanBarCodeCommand { get; }
+
         public MainPageViewModel(IBarCodeReaderService barCodeService)
         {
             _barCodeService = barCodeService;
+            ScanBarCodeCommand = new AsyncRelayCommand(ScanBarCode);
+        }
+
+        private Task ScanBarCode()
+        {
+            Debug.WriteLine("Hey, lets scan a bar code?");
+            return Task.CompletedTask;
         }
     }
 }
